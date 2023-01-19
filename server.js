45499a198a2b1333a192241aa1a078b1a9ddc6f4
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-20 00:59:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(express.json())
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
   // Handle the post request here
   console.log(req.body)
   res.send('Hello World')
