4872404113f825358457e270f405fbb1d59ac2aa
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-19 19:26:32 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.post('/', (req, res) => {
+  // Handle the post request here
+  console.log(req.body)
+  res.send('Hello World')
+})
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
+})
