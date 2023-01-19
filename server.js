c1b68964531c383f4ee55db0afa7257064939246
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-19 19:50:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -43,9 +43,40 @@ async function getTableValues() {
     ...data.map((row) => row[i + 1]),
   ]);
 
-  // Exclude the "Link", "Update Status", "In Stock", and "Last Stock" columns from the table output
+ // Exclude the "Link", "Update Status", "In Stock", and "Last Stock" columns from the table output
   const tableData = transposedData
     .filter(
       (row) =>
         row[0] !== "Link" &&
-        row[0
+        row[0] !== "Update Status" &&
+        row[0] !== "In Stock" &&
+        row[0] !== "Last Stock"
+    )
+    .map((row) => row.slice(1));
+
+  // Assign the "Link" column to value2
+  const links = transposedData.find((row) => row[0] === "Link");
+  const value2 = links ? links[1] : "";
+
+  console.log(table(transposedData));
+
+  await browser.close();
+
+  return { tableData, value2 };
+}
+
+async function sendNotification(value1, value2) {
+  await axios.post(
+    "https://maker.ifttt.com/trigger/send_notification/with/key/d_S45YgOUQspXBsB7VymKs",
+    { value1, value2 }
+  );
+}
+
+(async () => {
+  const { tableData, value2 } = await getTableValues();
+  const tableOutput = table(tableData, {
+    hsep: " | ",
+    stringLength: (str) => str.length,
+  });
+  await sendNotification(tableOutput, value2);
+})();
