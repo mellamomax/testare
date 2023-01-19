a3feb1d57eaf21f1013d68cbeeed6477a7db1207
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-19 23:51:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+app.use(express.json())
 
 app.post('/', (req, res) => {
   // Handle the post request here
