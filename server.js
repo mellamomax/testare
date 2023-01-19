68023a415d4680aa72cf51e7ac635d93aa086422
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-19 23:40:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,100 +1,13 @@
-const express = require("express");
-const app = express();
-const table = require("text-table");
-const puppeteer = require("puppeteer");
-const axios = require("axios");
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-const headers = [
-  "Description",
-  "Link",
-  "Update Status",
-  "Vendor",
-  "In Stock",
-  "Last Stock",
-  "Price",
-];
-
-async function getTableValues() {
-  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-  const page = await browser.newPage();
-  await page.goto("https://rpilocator.com/?cat=PI4");
-  await page.waitForSelector("#myTable");
-
-  const data = await page.evaluate(() => {
-    const rows = Array.from(document.querySelectorAll("#myTable tr")).filter(
-      (row) =>
-        row.classList.length !== 1 ||
-        !["odd", "even"].includes(row.classList[0])
-    );
-    return rows.map((row) => {
-      const cells = Array.from(row.querySelectorAll("td"));
-      return cells.map((cell) =>
-        cell.classList.contains("text-center")
-          ? cell.querySelector("a")
-            ? cell.querySelector("a").getAttribute("href")
-            : ""
-          : cell.textContent
-      );
-    });
-  });
-
-  const transposedData = [...headers].map((header, i) => [
-    header,
-    ...data.map((row) => row[i + 1]),
-  ]);
-
- // Exclude the "Link", "Update Status", "In Stock", and "Last Stock" columns from the table output
-  const tableData = transposedData
-    .filter(
-      (row) =>
-        row[0] !== "Link" &&
-        row[0] !== "Update Status" &&
-        row[0] !== "In Stock" &&
-        row[0] !== "Last Stock"
-    )
-    .map((row) => row.slice(1));
-
-  // Assign the "Link" column to value2
-  const links = transposedData.find((row) => row[0] === "Link");
-  const value2 = links ? links[1] : "";
-
-  console.log(table(transposedData));
-
-  await browser.close();
-
-  return { tableData, value2 };
-}
-
-async function sendNotification(value1, value2) {
-  await axios.post(
-    "https://maker.ifttt.com/trigger/send_notification/with/key/d_S45YgOUQspXBsB7VymKs",
-    { value1, value2 }
-  );
-}
-
-
-(async () => {
-  const { tableData, value2 } = await getTableValues();
-  const tableOutput = table(tableData, {
-    hsep: " | ",
-    stringLength: (str) => str.length,
-  });
-  await sendNotification(tableOutput, value2);
-})();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.post("/", async (req, res) => {
-  const { tableData, value2 } = await getTableValues();
-  const tableOutput = table(tableData, {
-    hsep: " | ",
-    stringLength: (str) => str.length,
-  });
-  await sendNotification(tableOutput, value2);
-  res.send("Script executed successfully!");
-});
-
+app.post('/', (req, res) => {
+  // Handle the post request here
+  console.log(req.body)
+  res.send('Hello World')
+})
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+  console.log('Server running on port 3000')
+})
