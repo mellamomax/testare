2b9e8d9e220ecce27172709e99319c2c58940764
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-19 19:49:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,51 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const table = require("text-table");
+const puppeteer = require("puppeteer");
+const axios = require("axios");
 
-app.post('/', (req, res) => {
-  // Handle the post request here
-  console.log(req.body)
-  res.send('Hello World')
-})
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
-})
+const headers = [
+  "Description",
+  "Link",
+  "Update Status",
+  "Vendor",
+  "In Stock",
+  "Last Stock",
+  "Price",
+];
+
+async function getTableValues() {
+  const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+  const page = await browser.newPage();
+  await page.goto("https://rpilocator.com/?cat=PI4");
+  await page.waitForSelector("#myTable");
+
+  const data = await page.evaluate(() => {
+    const rows = Array.from(document.querySelectorAll("#myTable tr")).filter(
+      (row) =>
+        row.classList.length !== 1 ||
+        !["odd", "even"].includes(row.classList[0])
+    );
+    return rows.map((row) => {
+      const cells = Array.from(row.querySelectorAll("td"));
+      return cells.map((cell) =>
+        cell.classList.contains("text-center")
+          ? cell.querySelector("a")
+            ? cell.querySelector("a").getAttribute("href")
+            : ""
+          : cell.textContent
+      );
+    });
+  });
+
+  const transposedData = [...headers].map((header, i) => [
+    header,
+    ...data.map((row) => row[i + 1]),
+  ]);
+
+  // Exclude the "Link", "Update Status", "In Stock", and "Last Stock" columns from the table output
+  const tableData = transposedData
+    .filter(
+      (row) =>
+        row[0] !== "Link" &&
+        row[0
