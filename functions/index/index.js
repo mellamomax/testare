2a205985d66628d6e3337abbf67f9f215c612431
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-22 21:32:16 +0100

    Update index.js

--- functions/index/index.js
+++ functions/index/index.js
@@ -1,17 +1,21 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
+    if (event.httpMethod === 'POST') {
+      const data = JSON.parse(event.body)
+      const subject = data.name || 'World'
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Hello ${subject} from post request` }),
+      }
+    }
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ message: `Hello ${subject} from get request` }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
 }
 
-module.exports = { handler }
