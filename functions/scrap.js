998a323c62c8afd3df8408bb6d00152771715790
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-24 00:34:21 +0100

    Update scrap.js

--- functions/scrap.js
+++ functions/scrap.js
@@ -1,17 +1,39 @@
 const puppeteer = require("puppeteer");
 
 exports.handler = async (event, context) => {
+    console.log("Event:", event);
+    console.log("Context:", context);
     if(event.httpMethod === "POST"){
-        const body = JSON.parse(event.body);
+        let body;
+        try {
+            body = JSON.parse(event.body);
+            console.log("Parsed Body:", body);
+        } catch (err) {
+            console.error("Error parsing JSON:", err);
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Invalid JSON in request body" }),
+            };
+        }
+        if (!body.url) {
+            console.error("Missing url property in request body");
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error: "Missing url property in request body" }),
+            };
+        }
         const url = body.url;
         let div;
         try {
+            console.log("Scraping URL:", url);
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
             await page.goto(url);
             div = await page.$eval(".col-md-6.col-md-offset-3", (el) => el.outerHTML);
+            console.log("Scraped HTML:", div);
             await browser.close();
         } catch (error) {
+            console.error("Error scraping website:", error);
             return {
                 statusCode: 400,
                 body: JSON.stringify({ error }),
@@ -22,6 +44,7 @@ exports.handler = async (event, context) => {
             body: JSON.stringify({ div }),
         };
     }else{
+        console.error("Bad request, only POST method is accepted.");
         return {
             statusCode: 400,
             body: JSON.stringify({ message: "Bad request, only POST method is accepted." }),
