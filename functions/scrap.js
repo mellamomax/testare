25aef78f157e05e3686a55f143a2138a4a8ee467
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-24 00:08:38 +0100

    Update scrap.js

--- functions/scrap.js
+++ functions/scrap.js
@@ -1,23 +1,22 @@
-const cheerio = require("cheerio");
-const request = require("request");
+const puppeteer = require("puppeteer");
 
 exports.handler = async (event, context) => {
   const url = "https://rpilocator.com/?cat=PI4";
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-        const table = $(".myTable").html();
-        resolve({
-          statusCode: 200,
-          body: JSON.stringify({ table }),
-        });
-      } else {
-        reject({
-          statusCode: 400,
-          body: JSON.stringify({ error }),
-        });
-      }
-    });
-  });
+  let table;
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    table = await page.$eval("#myTable", (el) => el.outerHTML);
+    await browser.close();
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error }),
+    };
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ table }),
+  };
 };
