7ba800049115c3e6262d8fd2f182a84da7b9706a
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-23 02:07:42 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,38 @@
-const express = require('express')
-const axios = require('axios')
-const app = express()
+const { spawnSync } = require('child_process');
 
-app.use(express.json()) // to parse JSON bodies
-
-app.post('/', (req, res) => {
-  // Use axios to make a POST request to the IFTTT webhook
-  axios.post('https://maker.ifttt.com/trigger/send_notification/with/key/d_S45YgOUQspXBsB7VymKs', req.body)
-    .then(response => {
-      res.send(response.data)
-    })
-    .catch(error => {
-      res.send(error)
-    })
-})
-
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
+const handler = async (event) => {
+  try {
+    if (event.httpMethod === 'POST') {
+      const result = spawnSync('node', ['server.js']);
+      if (result.error) {
+        console.error(`exec error: ${result.error}`);
+        return { statusCode: 500, body: result.error.toString() }
+      }
+      if (result.stderr) {
+        console.error(`stderr: ${result.stderr.toString()}`);
+      }
+      if (result.stdout) {
+        console.log(`stdout: ${result.stdout.toString()}`);
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Server script is completed" }),
+      };
+    } else if (event.httpMethod === 'GET') {
+      const subject = event.queryStringParameters.name || 'World'
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Hello ${subject} from get request` }),
+      }
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Invalid request method' }),
+      }
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
 }
+
+exports.handler = handler
