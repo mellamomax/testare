0c2a6651cc72b608db161472e69d56f92a674f5d
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-24 00:17:42 +0100

    Update scrap.js

--- functions/scrap.js
+++ functions/scrap.js
@@ -1,22 +1,29 @@
 const puppeteer = require("puppeteer");
 
 exports.handler = async (event, context) => {
-  const url = "https://www.scrapethissite.com/pages/";
-  let div;
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    div = await page.$eval(".col-md-6.col-md-offset-3", (el) => el.outerHTML);
-    await browser.close();
-  } catch (error) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error }),
-    };
-  }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ div }),
-  };
+    if(event.httpMethod === "POST"){
+        const url = JSON.parse(event.body).url;
+        let div;
+        try {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(url);
+            div = await page.$eval(".col-md-6.col-md-offset-3", (el) => el.outerHTML);
+            await browser.close();
+        } catch (error) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({ error }),
+            };
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ div }),
+        };
+    }else{
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "Bad request, only POST method is accepted." }),
+        };
+    }
 };
