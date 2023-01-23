22681fb70036bf575ebf4cfdd69133f8a1058098
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-23 20:08:11 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,16 @@
-const handler = async (event) => {
-  const axios = require('axios');
+const axios = require('axios');
+
+const postToIFTTTWebhook = async (body) => {
   try {
-    const postToIFTTTWebhook = async (body) => {
-       await axios.post('https://maker.ifttt.com/trigger/send_notification/with/key/d_S45YgOUQspXBsB7VymKs', body);
-    }
-    const data = {
-      value1: "example_data"
-    }
-    await postToIFTTTWebhook(data);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Successfully sent data to IFTTT webhook" })
-    }
+    const response = await axios.post('https://maker.ifttt.com/trigger/send_notification/with/key/d_S45YgOUQspXBsB7VymKs', body);
+    console.log(response.data);
   } catch (error) {
-    return {
-        statusCode: 500,
-        body: JSON.stringify({ error: error.toString() })
-    }
+    console.error(error);
   }
 }
+
+const data = {
+  value1: "example_data"
+}
+
+postToIFTTTWebhook(data);
