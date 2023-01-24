1b2c1d006e7f489a8283f6a9c85f751d402138ab
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-24 01:05:14 +0100

    Update scrap.js

--- functions/scrap.js
+++ functions/scrap.js
@@ -1,46 +1,22 @@
-const puppeteer = require("puppeteer");
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
 
-exports.handler = async (event, context) => {
-    if(event.httpMethod === "POST"){
-        let body;
-        try {
-            body = JSON.parse(event.body);
-        } catch (err) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error: "Invalid JSON in request body" }),
-            };
-        }
-        if (!body.url) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error: "Missing url property in request body" }),
-            };
-        }
-        const url = body.url;
-        let title;
-        try {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto(url);
-            title = await page.$eval("title", (el) => el.textContent);
-            console.log(`Title: ${title}`);
-            await browser.close();
-        } catch (error) {
-            console.log(error);
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error }),
-            };
-        }
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ title }),
-        };
-    }else{
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: "Bad request, only POST method is accepted." }),
-        };
+const app = express();
+
+app.get('/scrape', (req, res) => {
+  request('https://www.scrapethissite.com/pages/simple/', (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+
+      const title = $('h1').text();
+      const description = $('p').text();
+
+      res.json({ title, description });
     }
-};
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
