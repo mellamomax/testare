2dd1c1e2852517aa8d0a94997c5fb16a670ffe11
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-30 01:37:59 +0100

    Update scrap.js

--- functions/scrap.js
+++ functions/scrap.js
@@ -7,20 +7,20 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/scrap', (req, res) => {
-  const url = req.body.url;
-  request(url, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
-      const title = $('h1').text();
-      const description = $('p').text();
-      res.status(200).json({ title, description });
-    } else {
-      res.status(500).json({ error: 'Failed to scrape website' });
-    }
-  });
+console.log('Received POST request');
+const url = req.body.url;
+request(url, (error, response, html) => {
+if (!error && response.statusCode == 200) {
+console.log('Website successfully scraped');
+const $ = cheerio.load(html);
+const title = $('h1').text();
+const description = $('p').text();
+res.json({ title, description });
+} else {
+console.error('Failed to scrape website');
+res.status(500).json({ error: 'Failed to scrape website' });
+}
+});
 });
 
-
-exports.handler = (event, context, callback) => {
-  app(event, context, callback);
-};
+exports.handler = app;
