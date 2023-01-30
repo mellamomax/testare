bb9af1547c690c34a301217f884a56205c5ce1b2
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-30 12:25:23 +0100

    Update scrap.js

--- functions/scrap.js
+++ functions/scrap.js
@@ -58,7 +58,7 @@ statusCode: 200,
 headers: {
 "Content-Type": "text/plain; charset=utf-8",
 },
-body: [headers, ...filteredTableData]
+body: transposedData
 .map((row) => row.join("\t"))
 .join("\n"),
 };
