408f30979429d1d2889af0db9025f81071ad90a2
mellamomax <122839345+mellamomax@users.noreply.github.com>
2023-01-30 01:06:01 +0100

    Update scrap.js

--- functions/scrap.js
+++ functions/scrap.js
@@ -1,29 +1,21 @@
 const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.post('/functions/scrap', (req, res) => {
   const url = req.body.url;
   request(url, (error, response, html) => {
     if (!error && response.statusCode == 200) {
       const $ = cheerio.load(html);
-
       const title = $('h1').text();
       const description = $('p').text();
-
       res.json({ title, description });
     }
   });
 });
 
-
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
-const bodyParser = require('body-parser');
-
-// parse application/json
-app.use(bodyParser.json());
 exports.handler = app;
